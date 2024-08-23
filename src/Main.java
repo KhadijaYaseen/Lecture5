185275dcf661a545aa3f6d200cfd966c93d460c8
khadijayaseen <Khadijayaseen36@gmail.com>
2024-08-23 17:26:13 +0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        //caller
         System.out.println("hello");
+
+//functions..
+//        print();
+        //access modifier
+        //public everyone can access this function
+        //private only current class can access
+        //default can accessible in default package/folder
     }
+     public static void print(){
+        //collie
+         System.out.println("message 1");
+         print1();
+    }
+
+    private static void print1() {
+        System.out.println("message2");
+    }
+
+
 }
