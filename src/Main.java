bb4961a0176c5137fbf4d57de4060b1def8b76fa
khadijayaseen <Khadijayaseen36@gmail.com>
2024-08-27 18:31:21 +0500

    Factorial problem

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         //recursive
+        /* factorial
+        * 4!=4*3!
+        * 3!=3*2!
+        * 2!=2*1!
+        * 1!=1*0!
+        * 0!=1
+        *n!=n*(n-1)!
+        * */
+
+
+
+
         /*
         * int power (base , exp)
         * 2^4= 2*2^3
@@ -23,16 +35,28 @@ public class Main {
 
 
 //        printSeries(5);
-        System.out.println("Power ="+power(2,4 ));
+//        System.out.println("Power ="+power(2,4 ));
+        System.out.println("Factorial is "+fact(4 ));
     }
+    //find factorial
+    private static int fact(int num){
+        if(num==0||num==1)//base condition
+            return 1;
+   return num* fact(num-1);//recursive call
+    }
+
+
+
+
+
     //remaining code ;
 
 // power function using recursion
-static int power(int base,int  exp){
-
-        if (exp==0)//base condition
-            return 1;
-      return base* power(base,exp-1);
+//static int power(int base,int  exp){
+//
+//        if (exp==0)//base condition
+//            return 1;
+//      return base* power(base,exp-1);
 }
 
 
@@ -44,4 +68,3 @@ static int power(int base,int  exp){
 //         printSeries(num-1);// recursive call
 //         System.out.println(num);
 //    }
-}
